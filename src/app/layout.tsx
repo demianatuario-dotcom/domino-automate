1f3e1c5a6ab9bc06448b71e5884c86945e351d93
DEMIAN <demian.atuario@gmail.com>
2026-05-01 22:00:10 -0300

    feat: add Google Analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import React from 'react';
 
@@ -14,6 +15,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <Script 
+        async 
+        src="https://www.googletagmanager.com/gtag/js?id=G-NMQX5ZBVXX" 
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NMQX5ZBVXX');
+        `}
+      </Script>
       <body>
         <main>
           {children}
